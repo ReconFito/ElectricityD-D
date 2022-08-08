495c56441e34049a802d87d2e5df3178126e997a
Emanuel Maliaño <72898869+ReconFito@users.noreply.github.com>
2022-08-07 18:20:53 -0600

    Update __init__.py

--- if_dev/__init__.py
+++ if_dev/__init__.py
@@ -10,16 +10,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     app.app_context().push()
-    # septup enviroment variables for MAIL_ACCOUNT and MAIL_PASSWORD
-    # app.config['MAIL_SERVER']='smtp.gmail.com'
-    # app.config['MAIL_PORT'] = 465
-    # app.config['MAIL_USERNAME'] = "munozfito4@gmail.com"
-    # app.config['MAIL_PASSWORD'] = "gxxvralbrgefxtsx"
-    # app.config["MAIL_DEFAULT_SENDER"] = "munozfito4@gmail.com"
-    # app.config['MAIL_USERNAME'] = os.getenv("MAIL_ACCOUNT")
-    # app.config['MAIL_PASSWORD'] = os.getenv("MAILL_PASSWORD")
-    # app.config['MAIL_USE_TLS'] = False
-    # app.config['MAIL_USE_SSL'] = True
+    
     mail.init_app(app)
 
     from if_dev.routes import main
